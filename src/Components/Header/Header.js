442275313a146c0101cbd5c5227c4ff1c86e9177
Salman-S-Rahman <salmanmern@gmail.com>
2022-04-11 12:22:01 +0600

    protected route practice

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <h2 className="text-center">This is home.</h2>
+    </div>
+  );
+};
+
+export default Header;
